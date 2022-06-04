83505b4685d027e47d8b4a423aed5a54fba75e90
techboyg5 <63317676+techboyg5@users.noreply.github.com>
2022-06-04 17:34:41 -0500

    Initial Commit

---
+++ I Am Rich!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich!
+//
+//  Created by Jordan Nicodemus on 4/15/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
